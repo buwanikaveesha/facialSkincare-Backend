a257982ec58fde108e726392a983f92194130b1b
buwanikaveesha <buwanikaveesha@gmail.com>
2025-04-07 23:08:05 +0530

    cors update4

--- server.js
+++ server.js
@@ -10,12 +10,17 @@ import userRoutes from './routes/users.js';
 const app = express();
 const PORT = 3000;
 
-app.use(cors({
-  origin: "https://facial-skincare-frontend.vercel.app",
-  credentials: true
-}));
-
 app.use(express.json());
+app.use(cors({ origin: "*" }));
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 app.use("/api/auth", authRoutes);
 app.use("/api/user", userRoutes);
