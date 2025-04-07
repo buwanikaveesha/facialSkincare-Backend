969f580afe3fc487dc617699d0d64b7b704d643a
buwanikaveesha <buwanikaveesha@gmail.com>
2025-04-07 22:58:59 +0530

    cors update2

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-import express from "express";
-import "dotenv/config";
 import cors from "cors";
+import "dotenv/config";
+import express from "express";
 import DBConnection from "./config/dbConnection.js";
 import authRoutes from './routes/auth.js';
-import userRoutes from './routes/users.js';
-import resultRoutes from './routes/result.js';
 import feedbackRoutes from './routes/feedback.js';
+import resultRoutes from './routes/result.js';
+import userRoutes from './routes/users.js';
 
 const app = express();
 const PORT = 3000;
