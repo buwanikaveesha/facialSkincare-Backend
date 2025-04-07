437732399a4ee2791d315b21966f63fc2e3678da
buwanikaveesha <buwanikaveesha@gmail.com>
2025-04-07 22:56:53 +0530

    cors update

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ import feedbackRoutes from './routes/feedback.js';
 const app = express();
 const PORT = 3000;
 
-app.use(cors());
+app.use(cors({ origin: "*" }));
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
