339739b710691a133a92911c121611ba3e0eb184
buwanikaveesha <buwanikaveesha@gmail.com>
2025-04-09 13:39:29 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,16 +10,19 @@ import userRoutes from './routes/users.js';
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Define CORS options
 const corsOptions = {
   origin: 'https://facial-skincare-frontend.vercel.app',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization', 'x-requested-with'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
 };
 
+// Use CORS middleware
 app.use(cors(corsOptions));
 app.options('*', cors(corsOptions));
 
+// Middleware
 app.use(express.json());
 
 // Routes
@@ -28,6 +31,7 @@ app.use("/user", userRoutes);
 app.use("/result", resultRoutes);
 app.use("/feedback", feedbackRoutes);
 
+// Start server
 app.listen(PORT, () => {
   DBConnection();
   console.log(`Server is running on port ${PORT}`);
