9b1c51dbd0b4fc6c017304ea511ba7fe9bcb3bbf
buwanikaveesha <buwanikaveesha@gmail.com>
2025-04-11 19:55:55 +0530

    remove vercel.json

--- server.js
+++ server.js
@@ -10,16 +10,6 @@ import userRoutes from './routes/users.js';
 const app = express();
 const PORT = 3000;
 
-// app.use(cors({ origin: "*" }));
-// app.use(function (req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Methods", "GET, PUT, POST");
-//   res.header(
-//     "Access-Control-Allow-Headers",
-//     "Origin, X-Requested-With, Content-Type, Accept"
-//   );
-//   next();
-// });
 app.use(cors());
 app.use(express.json());
 
